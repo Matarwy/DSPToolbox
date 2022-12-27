081984a8c092f4af191dad449f070e6204afd235
Alhassan Matarwy <67296782+Matarwy@users.noreply.github.com>
2022-12-27 15:24:04 +0200

    last task

---
+++ DSPComponents/Algorithms/DCT.cs
@@ -0,0 +1,36 @@
+﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class DCT: Algorithm
+    {
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+        public override void Run()
+        {
+            List<float> values = new List<float>();
+            int N = InputSignal.Samples.Count();
+            double alpha = Math.Sqrt((double)2 / N);
+
+            for (int k = 0; k < N; k++)
+            {
+                float sum = 0;
+
+                for (int n = 0; n < N; n++)
+                {
+                    sum += (float)InputSignal.Samples[n] * (float)Math.Cos(Math.PI / (4 * N) * (2 * n - 1) * (2 * k - 1));
+                }
+
+                values.Add((float)alpha * sum);
+            }
+
+            OutputSignal = new Signal(values, false);
+        }
+    }
+}
--- DSPComponents/Algorithms/DirectCorrelation.cs
+++ DSPComponents/Algorithms/DirectCorrelation.cs
@@ -16,7 +16,106 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+
+
+            //  Correlation : measuring of similarity (dependency) between two signals
+
+
+
+            List<float> list_signal1 = new List<float>();
+            List<float> list_signal2 = new List<float>();
+            List<float> corr = new List<float>();
+            List<float> normalizecorr = new List<float>();
+
+
+
+            float sum1 = 0, sum2 = 0, norm_sum = 0;
+            int N;
+
+
+
+            if (InputSignal2 != null)
+            {
+                // cross correlation ...
+                N = InputSignal1.Samples.Count;
+
+
+
+                list_signal1 = InputSignal1.Samples;
+                list_signal2 = InputSignal2.Samples;
+
+            }
+            else
+            {
+                //AUTO CORRELACTION ...
+                N = InputSignal1.Samples.Count;
+
+
+
+                list_signal1 = InputSignal1.Samples;
+                list_signal2 = InputSignal1.Samples;
+            }
+
+
+
+            // Normalization cross correlation ...
+            for (int i = 0; i < N; i++)
+            {
+                sum1 = sum1 + (float)Math.Pow(list_signal1[i], 2);
+                sum2 = sum2 + (float)Math.Pow(list_signal2[i], 2);
+            }
+            norm_sum = (float)Math.Sqrt((sum1 * sum2)) / N;
+            //...........
+
+            if (InputSignal1.Periodic == false)
+            {
+                // Not Periodic
+                for (int j = 0; j < N; j++)
+                {
+                    float x = 0;
+                    for (int n = 0; n < N - j; n++)
+                    {
+                        // sum( x1(n)*x2(n+j) )/N ...
+                        x = x + (list_signal1[n] * list_signal2[n + j]);
+                    }
+                    corr.Add(x / N);
+                }
+            }
+            else
+            {
+                // periodic
+                for (int i = 0; i < N; i++)
+                {
+                    float x = 0;
+                    int inc = 0;
+                    for (int j = 0; j < N; j++)
+                    {
+                        if (i + j >= N)
+                        {
+                            x = x + (list_signal1[j] * list_signal2[inc]);
+                            // to increase shift ...
+                            inc++;
+                        }
+                        else
+                        {
+                            // sum( x1(n)*x2(n+j) )/N ...
+                            x = x + (list_signal1[j] * list_signal2[j + i]);
+                        }
+                    }
+                    corr.Add(x / N);
+                }
+            }
+
+            for (int i = 0; i < corr.Count; i++)
+            {
+                normalizecorr.Add(corr[i] / norm_sum);
+            }
+
+            OutputNonNormalizedCorrelation = corr;
+            OutputNormalizedCorrelation = normalizecorr;
+
         }
     }
 }
